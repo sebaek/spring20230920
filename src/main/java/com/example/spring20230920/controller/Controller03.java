792afbb5c208f33a0a5c773580fa3670c5eab5e3
sebaek <sebaek@gmail.com>
2023-09-21 10:27:42 +0900

    request parameter

--- src/main/java/com/example/spring20230920/controller/Controller03.java
+++ src/main/java/com/example/spring20230920/controller/Controller03.java
@@ -2,6 +2,7 @@ package com.example.spring20230920.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.request.WebRequest;
 
 @Controller
@@ -19,4 +20,32 @@ public class Controller03 {
         System.out.println("address = " + address);
     }
 
+
+    // /main2/sub2?name=son&address=london
+    @RequestMapping("sub2")
+    public void method2(WebRequest request) {
+        String name = request.getParameter("name");
+        String address = request.getParameter("address");
+
+        System.out.println("name = " + name);
+        System.out.println("address = " + address);
+    }
+
+    // /main2/sub3?name=손흥민
+    @RequestMapping("sub3")
+    public void method3(@RequestParam("name") String name) {
+        System.out.println("name = " + name);
+    }
+
+    // /main2/sub4?name=김두식&address=서울
+    @RequestMapping("sub4")
+    public void method4(@RequestParam("name") String name,
+                        @RequestParam("address") String address) {
+        System.out.println("name = " + name);
+        System.out.println("address = " + address);
+    }
+
+    // /main2/sub5?email=abc@gmail.com&gender=남자&number=010-9999
+    // method5 작성
+    
 }
