05953fc253f2092faa09f0512aa9b5a11ced2da8
sebaek <sebaek@gmail.com>
2023-09-21 14:22:08 +0900

    property

---
+++ src/main/java/com/example/spring20230920/domain/MyDto1.java
@@ -0,0 +1,54 @@
+package com.example.spring20230920.domain;
+
+public class MyDto1 {
+    // property 명 : get/set 없애고 소문자로 시작
+
+    // name property
+    // homeAddress property
+
+    private String name;
+    private String homeAddress;
+    private String MYHOME;
+    private Boolean married;
+    private boolean checked;
+
+    public boolean isChecked() {
+        return checked;
+    }
+
+    public void setChecked(boolean checked) {
+        this.checked = checked;
+    }
+
+    public Boolean getMarried() {
+        return married;
+    }
+
+    public void setMarried(Boolean married) {
+        this.married = married;
+    }
+
+    public String getMYHOME() {
+        return MYHOME;
+    }
+
+    public void setMYHOME(String MYHOME) {
+        this.MYHOME = MYHOME;
+    }
+
+    public String getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(String homeAddress) {
+        this.homeAddress = homeAddress;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
