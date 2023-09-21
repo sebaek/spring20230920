073b0da4b26c443223602d47d80b4a3308bfb664
sebaek <sebaek@gmail.com>
2023-09-21 12:25:50 +0900

    bean

--- src/main/java/com/example/spring20230920/controller/Controller05.java
+++ src/main/java/com/example/spring20230920/controller/Controller05.java
@@ -1,5 +1,6 @@
 package com.example.spring20230920.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,5 +15,63 @@ public class Controller05 {
         System.out.println("name = " + name);
         System.out.println("age = " + age);
         System.out.println("address = " + address);
+    }
+
+    // /main5/sub2?name=lee&age=44&address=paris
+    @RequestMapping("sub2")
+    public void method2(String name, Integer age, String address) {
+        Person5 o1 = new Person5();
+        o1.setName(name);
+        o1.setAge(age);
+        o1.setAddress(address);
+
+        System.out.println(o1);
+
+//        service.action(o1);
+    }
+
+    // /main5/sub3?name=lee&age=44&address=paris
+    @RequestMapping("sub3")
+    public void method3(Person5 person5) {
+        System.out.println("person5 = " + person5);
+    }
+}
+
+class Person5 {
+    private String name;
+    private Integer age;
+    private String address;
+
+    @Override
+    public String toString() {
+        return "Person5{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 }
