82e7c3e8960dd15ec3e7a6be7ff58e393ac98f13
sebaek <sebaek@gmail.com>
2023-09-25 14:06:46 +0900

    for, if

--- src/main/java/com/example/spring20230920/controller/Controller11.java
+++ src/main/java/com/example/spring20230920/controller/Controller11.java
@@ -1,7 +1,13 @@
 package com.example.spring20230920.controller;
 
+import com.example.spring20230920.domain.MyDto9;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("main11")
@@ -13,8 +19,30 @@ public class Controller11 {
     @RequestMapping("sub2")
     public void method2() {
     }
+
     @RequestMapping("sub3")
-    public void method3() {}
+    public void method3() {
+    }
+
     @RequestMapping({"sub4", "sub5", "sub6"})
-    public void method4() {}
+    public void method4() {
+    }
+
+    @RequestMapping("sub7")
+    public void method5(
+            @RequestParam(value = "show", defaultValue = "false") Boolean show,
+            Model model) {
+
+        if (show) {
+            List<MyDto9> list = new ArrayList<>();
+            list.add(new MyDto9(31L, "son", "hm", "football", "010"));
+            list.add(new MyDto9(32L, "lee", "ki", "paris", "011"));
+            list.add(new MyDto9(33L, "김", "두식", "서울", "02"));
+            list.add(new MyDto9(34L, "김", "봉석", "부산", "03"));
+            list.add(new MyDto9(35L, "장", "희수", "제주", "05"));
+
+            model.addAttribute("moving", list);
+        }
+
+    }
 }
---
+++ src/main/webapp/WEB-INF/jsp/main11/sub7.jsp
@@ -0,0 +1,21 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: admin
+  Date: 2023-09-25
+  Time: 오후 2:01
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="jakarta.tags.core" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<%-- /main11/sub7?show=true --%>
+<%-- table 형식의 데이터 출력 --%>
+
+<%-- /main11/sub7--%>
+<%-- 데이터가 없습니다. --%>
+</body>
+</html>
