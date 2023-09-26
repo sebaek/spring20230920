b4154bf314b4e27336ac692121650c4b1d456536
sebaek <sebaek@gmail.com>
2023-09-26 14:49:32 +0900

    get /post

--- src/main/java/com/example/spring20230920/controller/Controller14.java
+++ src/main/java/com/example/spring20230920/controller/Controller14.java
@@ -2,6 +2,7 @@ package com.example.spring20230920.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -12,11 +13,24 @@ public class Controller14 {
 
     }
 
-    @RequestMapping("sub2")
+    @RequestMapping(value = "sub2", method = RequestMethod.GET)
     public void method2(
             @RequestParam("id") String id,
             @RequestParam("password") String password
     ) {
+        System.out.println("Controller14.method2");
+
+        System.out.println("id = " + id);
+        System.out.println("password = " + password);
+    }
+
+    @RequestMapping(value = "sub2", method = RequestMethod.POST)
+    public void method3(
+            @RequestParam("id") String id,
+            @RequestParam("password") String password
+    ) {
+        System.out.println("Controller14.method3");
+
         System.out.println("id = " + id);
         System.out.println("password = " + password);
     }
