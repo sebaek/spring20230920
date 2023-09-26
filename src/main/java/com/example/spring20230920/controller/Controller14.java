dfe76a6be740dd2a2eb6ea4e4ffcfaa1ddded68d
sebaek <sebaek@gmail.com>
2023-09-26 15:32:24 +0900

    get/post

--- src/main/java/com/example/spring20230920/controller/Controller14.java
+++ src/main/java/com/example/spring20230920/controller/Controller14.java
@@ -1,20 +1,19 @@
 package com.example.spring20230920.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("main14")
 public class Controller14 {
-    @RequestMapping({"sub1",
+    @GetMapping({"sub1",
             "sub3"})
     public void method1() {
 
     }
 
-    @RequestMapping(value = "sub2", method = RequestMethod.GET)
+//    @RequestMapping(value = "sub2", method = RequestMethod.GET)
+    @GetMapping("sub2")
     public void method2(
             @RequestParam("id") String id,
             @RequestParam("password") String password
@@ -25,7 +24,8 @@ public class Controller14 {
         System.out.println("password = " + password);
     }
 
-    @RequestMapping(value = "sub2", method = RequestMethod.POST)
+//    @RequestMapping(value = "sub2", method = RequestMethod.POST)
+    @PostMapping("sub2")
     public void method3(
             @RequestParam("id") String id,
             @RequestParam("password") String password
@@ -36,12 +36,28 @@ public class Controller14 {
         System.out.println("password = " + password);
     }
 
-    @RequestMapping(value = "sub4", method = RequestMethod.POST)
+//    @RequestMapping(value = "sub4", method = RequestMethod.POST)
+    @PostMapping("sub4")
     public void method4(
             String title,
             String content
     ) {
         System.out.println("title = " + title);
         System.out.println("content = " + content);
+    }
+
+    // 1)아래 두 RequestMapping 어노테이션을 줄여서 작성
+    // 2)sub5.jsp에 적절한 form 요소 코드 작성해서
+    // 전송(submit)시 method6이 실행될 수 있도록....
+    @RequestMapping(value = "sub5", method = RequestMethod.GET)
+    public void method5() {}
+
+    @RequestMapping(value = "sub6", method = RequestMethod.POST)
+    public void method6(
+            String username,
+            String password
+    ) {
+        System.out.println("username = " + username);
+        System.out.println("password = " + password);
     }
 }
