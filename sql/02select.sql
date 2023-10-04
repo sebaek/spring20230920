31982de6966efaa415e5334996376ff5d2689cab
sebaek <sebaek@gmail.com>
2023-10-04 10:54:45 +0900

    distinct, count

--- sql/02select.sql
+++ sql/02select.sql
@@ -38,3 +38,22 @@ FROM products;
 -- 예3) 공급자(suppliers)의 이름(supplierName), 주소(address), 국가(country) 조회
 SELECT SupplierName, Address, Country
 FROM suppliers;
+
+
+-- 고객테이블의 국가 컬럼 조회
+SELECT Country FROM customers; -- 91개 row(record)
+
+-- DISTINCT 중복 제거
+SELECT DISTINCT Country FROM customers; -- 21개 row
+
+SELECT City, Country FROM customers; -- 91개
+SELECT DISTINCT City, Country FROM customers; -- 69개
+
+-- COUNT : 행(row, record)의 수
+SELECT COUNT(Country) FROM customers; -- 91
+SELECT COUNT(DISTINCT Country) FROM customers; -- 21
+
+-- 전체 행의 수
+SELECT COUNT(*) FROM customers;
+SELECT COUNT(*) FROM employees;
+SELECT COUNT(*) FROM orders;
