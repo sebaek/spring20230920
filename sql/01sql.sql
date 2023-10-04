10ce238442322079c1c58b8199066d268fb900fd
sebaek <sebaek@gmail.com>
2023-10-04 10:40:28 +0900

    select

---
+++ sql/01sql.sql
@@ -0,0 +1,14 @@
+-- 주석
+# 주석
+select * from w3schools.customers;
+select * from w3schools.employees;
+select *
+from w3schools.customers;
+-- sql 은 대소문자 구분안함
+SELECT *
+FROM W3SCHOOLS.CUSTOMERS;
+-- 키워드는 대문자, 나머지는 소문자 (관습, 엄격하지 않음)
+SELECT *
+FROM w3schools.customers;
+select *
+from w3schools.customers;
---
+++ sql/02select.sql
@@ -0,0 +1,34 @@
+-- SELECT : 데이터 조회(읽기, 검색, 가져오기)
+SELECT * FROM w3schools.customers;
+SELECT * FROM w3schools.employees;
+SELECT * FROM w3schools.products;
+
+SELECT * FROM customers;
+SELECT * FROM customers;
+
+SELECT * FROM categories;
+SELECT * FROM orderdetails;
+SELECT * FROM orders;
+SELECT * FROM products;
+SELECT * FROM shippers;
+SELECT * FROM suppliers;
+
+
+--
+SELECT CustomerName  -- column 명 나열
+FROM customers-- table 명 나열
+;
+
+SELECT CustomerID, CustomerName
+FROM customers;
+SELECT CustomerID, CustomerName, Country
+FROM customers;
+SELECT CustomerName, CustomerID, Country
+FROM customers;
+
+SELECT * -- 전체 컬럼
+FROM customers;
+
+-- 예1) 직원(employees)의 lastName, firstName, birthDate 조회
+-- 예2) 상품(products)의 이름(productName), 단위(unit), 가격(price) 조회
+-- 예3) 공급자(suppliers)의 이름(supplierName), 주소(address), 국가(country) 조회
