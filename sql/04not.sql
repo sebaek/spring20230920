4ef908e7acd18ea07ece0656c47ceca927886f30
sebaek <sebaek@gmail.com>
2023-10-05 12:21:35 +0900

    not

---
+++ sql/04not.sql
@@ -0,0 +1,7 @@
+SELECT * FROM customers;
+SELECT CustomerName, Country FROM customers WHERE Country = 'Germany';
+SELECT CustomerName, Country FROM customers WHERE NOT Country = 'Germany';
+
+-- 예) 1번 카테고리에 속하지 않는 상품들 조회
+-- 예) Japan 에 있지 않는 공급자들 조회
+
