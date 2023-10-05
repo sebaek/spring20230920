e6863471b82f48465d061bc30209caeb6ab67ee8
sebaek <sebaek@gmail.com>
2023-10-05 14:15:58 +0900

    and or

--- sql/06andor.sql
+++ sql/06andor.sql
@@ -26,3 +26,12 @@ SELECT CustomerName, City, Country FROM customers
 WHERE City = 'Madrid' OR Country = 'Spain';
 SELECT CustomerName, City, Country FROM  customers
 WHERE Country = 'Spain' OR Country = 'UK';
+
+SELECT * FROM products
+WHERE Price >= 10.00 AND Price <= 20.00;
+SELECT * FROM customers
+WHERE CustomerName >= 'c' AND CustomerName < 'd';
+SELECT * FROM employees
+WHERE BirthDate >= '1958-01-01' AND BirthDate < '1959-01-01';
+
+--
