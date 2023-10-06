f51b68a3a699aec1c11ea503f0b82fb88a554532
sebaek <sebaek@gmail.com>
2023-10-06 10:55:07 +0900

    order by

--- sql/11orderby.sql
+++ sql/11orderby.sql
@@ -27,3 +27,17 @@ SELECT * FROM products
 ORDER BY CategoryID DESC, Price;
 SELECT * FROM products
 ORDER BY CategoryID DESC, Price DESC;
+
+--  예) 고객을 국가명 오름차순으로 조회
+SELECT * FROM customers
+ORDER BY Country;
+--  예) 가격이 비싼 상품이 먼저 조회되도록 코드 작성
+SELECT * FROM products
+ORDER BY Price DESC ;
+-- 예) 가장 어린 직원이 먼저 조회되도록 코드 작성
+SELECT * FROM employees
+ORDER BY BirthDate DESC ;
+
+SELECT * FROM products
+WHERE CategoryID = 1
+ORDER BY Price DESC ;
