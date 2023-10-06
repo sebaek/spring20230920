bf5e6312d7ccc28ceeb0e2ae1db9daf12bb0078f
sebaek <sebaek@gmail.com>
2023-10-06 12:22:08 +0900

    add model attribute

--- src/main/java/com/example/spring20230920/controller/Controller22.java
+++ src/main/java/com/example/spring20230920/controller/Controller22.java
@@ -2,6 +2,7 @@ package com.example.spring20230920.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +12,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @RequestMapping("main22")
 @Controller
@@ -52,7 +56,8 @@ public class Controller22 {
     // 페이지당 7개 씩 공급자 목록 조회 (공급자 번호 순)
     // /main22/sub2?p=2
     @GetMapping("sub2")
-    public void method2(@RequestParam("p") Integer page) throws SQLException {
+    public void method2(@RequestParam("p") Integer page,
+                        Model model) throws SQLException {
         String sql = """
                 SELECT supplierId id,
                        supplierName name
@@ -69,14 +74,17 @@ public class Controller22 {
 
         ResultSet resultSet = statement.executeQuery();
 
+        List<Map<String, Object>> list = new ArrayList<>();
         try (connection; statement; resultSet) {
-            System.out.println(page + "페이지 공급자 목록");
+//            System.out.println(page + "페이지 공급자 목록");
             while (resultSet.next()) {
                 String id = resultSet.getString("id");
                 String name = resultSet.getString("name");
 
-                System.out.println(id + " : " + name);
+                list.add(Map.of("id", id, "name", name));
+//                System.out.println(id + " : " + name);
             }
         }
+        model.addAttribute("supplierList", list);
     }
 }
---
+++ src/main/webapp/WEB-INF/jsp/main22/sub2.jsp
@@ -0,0 +1,40 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: admin
+  Date: 2023-10-06
+  Time: 오후 12:20
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="jakarta.tags.core" %>
+<html>
+<head>
+    <title>Title</title>
+    <style>
+        table {
+            border-collapse: collapse;
+
+        }
+
+        th, td {
+            border: 1px solid black;
+        }
+    </style>
+</head>
+<body>
+<h5>공급자 목록</h5>
+<table>
+    <tr>
+        <th>id</th>
+        <th>name</th>
+    </tr>
+    <c:forEach items="${supplierList}" var="supplier">
+        <tr>
+            <td>${supplier.id}</td>
+            <td>${supplier.name}</td>
+        </tr>
+    </c:forEach>
+</table>
+
+</body>
+</html>
