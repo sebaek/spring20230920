05a424b4781f8168722d5a83c34f15baec4f1ca8
sebaek <sebaek@gmail.com>
2023-10-06 09:42:31 +0900

    산술연산, 문자열 연결연산, 날짜 변경연산

---
+++ sql/09arithmetic.sql
@@ -0,0 +1,28 @@
+SELECT price FROM products WHERE ProductID = 1;
+SELECT price + 1 FROM products WHERE ProductID = 1;
+SELECT price - 10.5 FROM products WHERE ProductID = 1;
+SELECT price * 3 FROM products WHERE ProductID = 1;
+SELECT price / 5 FROM products WHERE ProductID = 1;
+
+SELECT ProductName, Price * 2 FROM products;
+
+SELECT ProductName + 1, Price FROM products; -- 문자열 연결연산은 안됨
+SELECT CONCAT(ProductName, 1), Price FROM products; -- 문자열 연결은 concat 사용
+
+-- 1968-12-08
+SELECT LastName, BirthDate FROM employees WHERE EmployeeID = 1;
+-- 19681209
+SELECT LastName, BirthDate + 1 FROM employees WHERE EmployeeID = 1;
+SELECT LastName, ADDDATE(BirthDate, INTERVAL 1 DAY )
+FROM employees
+WHERE EmployeeID = 1; -- 날짜변경 함수 사용
+
+SELECT * FROM employees;
+SELECT EmployeeID, LastName, FirstName FROM employees;
+SELECT EmployeeID, CONCAT(LastName, FirstName) FROM employees;
+SELECT EmployeeID, CONCAT(LastName, ' ', FirstName) FROM employees;
+SELECT EmployeeID, CONCAT(FirstName, ' ', LastName) FROM employees;
+SELECT EmployeeID,
+       CONCAT(FirstName, ' ', LastName),
+       ADDDATE(BirthDate, INTERVAL 1 YEAR )
+FROM employees;
