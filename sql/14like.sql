8580542fb6ad393f3fb057505087681e4d4fb8be
sebaek <sebaek@gmail.com>
2023-10-10 09:40:21 +0900

    search

--- sql/14like.sql
+++ sql/14like.sql
@@ -6,7 +6,7 @@ SELECT * FROM customers
 WHERE CustomerName LIKE '%ch%';
 
 -- LIKE : 검색
--- %, _ 기호와 같이
+-- %, _ 기호(wildcard)와 같이
 SELECT * FROM customers
 WHERE CustomerName LIKE 'ch%'; -- % : 0개 이상의 문자
 SELECT * FROM customers
@@ -23,4 +23,16 @@ WHERE LastName LIKE '____'; -- _ : 1개의 문자 아무거나
 SELECT * FROM employees
 WHERE LastName LIKE '_e%';
 
+-- 예) ch로 시작하는 상품명 조회
+SELECT * FROM products
+WHERE ProductName LIKE 'ch%';
+-- 예) es로 끝나는 상품명 조회
+SELECT * FROM products
+WHERE ProductName LIKE '%es';
+-- 예) 중간에 use가 들어간 상품명 조회
+SELECT * FROM products
+WHERE ProductName LIKE '%use%';
 
+-- 예) 두번째 글자가 u인 상품명 조회
+SELECT * FROM products
+WHERE ProductName LIKE '_u%';
---
+++ src/main/java/com/example/spring20230920/controller/Controller23.java
@@ -0,0 +1,42 @@
+package com.example.spring20230920.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Controller
+@RequestMapping("main23")
+public class Controller23 {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @GetMapping("sub1")
+    public void method1(String keyword) throws SQLException {
+        String sql = """
+                SELECT * FROM products
+                WHERE productName LIKE ?
+                """;
+
+        Connection connection = dataSource.getConnection();
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setString(1, "%" + keyword + "%");
+        ResultSet resultSet = statement.executeQuery();
+
+        try (connection; statement; resultSet) {
+            System.out.println();
+            System.out.println(keyword + "가 포함된 상품명들");
+            while (resultSet.next()) {
+                String name = resultSet.getString("productName");
+                System.out.println("name = " + name);
+            }
+        }
+    }
+}
