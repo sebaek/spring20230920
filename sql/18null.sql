5343df42ef9ddce6d76cb83f837a3524218b2ae8
sebaek <sebaek@gmail.com>
2023-10-11 14:37:02 +0900

    null

--- sql/18null.sql
+++ sql/18null.sql
@@ -19,7 +19,7 @@ SELECT * FROM products
 WHERE Price != 0.00
 ORDER BY 1 DESC;
 
--- 피연산자가 NULL이면 결과는 항상 false
+-- 비교연산시 피연산자가 NULL이면 결과는 항상 false
 SELECT * FROM products
 WHERE Price IS NULL; -- IS NULL : 컬럼의 값이 null인지 확인
 SELECT * FROM products
@@ -27,4 +27,13 @@ WHERE NOT Price IS NULL ;
 SELECT * FROM products
 WHERE Price IS NOT NULL;
 
+SELECT ProductName, Price * 100
+FROM products; -- 산술연산시 null 피연산자이면 결과 null
 
+SELECT ProductName, Price
+FROM products
+ORDER BY ProductID DESC;
+
+SELECT ProductName, IFNULL(Price, 0.00)
+FROM products
+ORDER BY ProductID DESC ;
