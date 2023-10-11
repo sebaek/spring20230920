9120c23cf4005462c41c9506d1631905d9c401ba
sebaek <sebaek@gmail.com>
2023-10-11 15:14:39 +0900

    create table

--- sql/20create-table.sql
+++ sql/20create-table.sql
@@ -12,3 +12,17 @@ CREATE TABLE 테이블명 (
 -- UpperCamelCase
 -- lowerCamelCase
 
+CREATE TABLE my_table1 (
+  col_name1 varchar(100),
+  col_name2 varchar(100),
+  col_name3 varchar(100)
+);
+
+SELECT * FROM my_table1;
+INSERT INTO my_table1 (col_name1, col_name2, col_name3)
+VALUE ('민재', '흥민', '강인');
+
+-- 예) my_table2 만들기, 컬럼은 4개, 모든 컬럼 타입 varchar(10)
+
+
+
