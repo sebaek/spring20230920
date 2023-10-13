5651864219118b5d2c719f4e3efe68f3110bcd49
sebaek <sebaek@gmail.com>
2023-10-13 10:35:18 +0900

    join, outer join

--- sql/31join.sql
+++ sql/31join.sql
@@ -11,3 +11,13 @@ FROM customers c LEFT JOIN orders o
      ON c.CustomerID = o.CustomerID
 WHERE o.CustomerID IS NULL ;
 
+SELECT e.LastName, e.FirstName
+FROM employees e LEFT JOIN orders o
+    ON e.EmployeeID = o.EmployeeID
+WHERE o.EmployeeID IS NULL ;
+
+SELECT DISTINCT EmployeeID FROM orders ORDER BY 1;
+SELECT COUNT(*) FROM employees;
+
+INSERT INTO employees (LastName, FirstName, BirthDate, Photo, Notes)
+VALUE ('손', '흥민', '2000-01-01', '사진1', '축구선수');
