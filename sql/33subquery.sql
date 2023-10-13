aa41e452c47188befb1bc54696e23120424fcb4e
sebaek <sebaek@gmail.com>
2023-10-13 11:38:07 +0900

    subquery

--- sql/33subquery.sql
+++ sql/33subquery.sql
@@ -44,3 +44,25 @@ WHERE SupplierID = (SELECT SupplierID FROM products WHERE ProductName = 'tofu');
 SELECT p.SupplierID, s.SupplierName FROM suppliers s JOIN products p ON s.SupplierID = p.SupplierID
 WHERE p.ProductName = 'tofu';
 
+-- 여러행 1개열
+-- 멕시코 고객이 주문한 일자들 조회
+SELECT CustomerID FROM customers WHERE Country = 'mexico';
+SELECT OrderDate FROM orders WHERE CustomerID IN (2, 3, 13, 58, 80);
+
+SELECT OrderDate FROM orders
+WHERE CustomerID IN (SELECT CustomerID FROM customers WHERE Country = 'mexico');
+
+-- 예) 1번 카테고리에 있는 상품이 주문된 주문번호 (orderDetails, products)
+SELECT ProductID FROM products WHERE CategoryID = 1;
+
+SELECT OrderID FROM orderdetails
+         WHERE ProductID IN (SELECT ProductID FROM products WHERE CategoryID = 1);
+-- 예) 1번 카테고리에 있는 상품이 주문된 날짜 (orderDetails, products)
+SELECT OrderDate FROM orders
+WHERE OrderID IN (SELECT OrderID FROM orderdetails
+                  WHERE ProductID IN (SELECT ProductID FROM products WHERE CategoryID = 1));
+
+SELECT *
+FROM orders o JOIN orderdetails od ON o.OrderID = od.OrderID
+     JOIN products p ON od.ProductID = p.ProductID
+WHERE p.CategoryID = 1;
