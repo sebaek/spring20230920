11b73ec5266fe207a9c2237b97099fe80a298f51
sebaek <sebaek@gmail.com>
2023-10-16 10:52:17 +0900

    having

--- sql/37groupby.sql
+++ sql/37groupby.sql
@@ -53,3 +53,11 @@ ORDER BY 2 DESC;
 
 
 
+-- 10만 달러 이상 소비한 고객 조회
+SELECT * FROM
+    (SELECT c.CustomerName, SUM(p.Price * od.Quantity) `총 주문금액`
+    FROM customers c JOIN orders o ON c.CustomerID = o.CustomerID
+                     JOIN orderdetails od ON o.OrderID = od.OrderID
+                     JOIN products p ON od.ProductID = p.ProductID
+    GROUP BY c.CustomerID) t1
+WHERE t1.`총 주문금액` >= 100000;
---
+++ sql/38having.sql
@@ -0,0 +1,10 @@
+-- HAVING : 그룹함수 연산결과를 조건으로 줄 수 있는 키워드
+
+-- 10만 달러 이상 소비한 고객 조회
+SELECT * FROM
+    (SELECT c.CustomerName, SUM(p.Price * od.Quantity) `총 주문금액`
+     FROM customers c JOIN orders o ON c.CustomerID = o.CustomerID
+                      JOIN orderdetails od ON o.OrderID = od.OrderID
+                      JOIN products p ON od.ProductID = p.ProductID
+     GROUP BY c.CustomerID) t1
+WHERE t1.`총 주문금액` >= 100000;
