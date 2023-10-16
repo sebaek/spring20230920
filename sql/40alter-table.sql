2e12ab9267cd9972d1f9e80326a05cc1d3f3f87c
sebaek <sebaek@gmail.com>
2023-10-16 14:08:37 +0900

    default 제약사항 추가 삭제

--- sql/40alter-table.sql
+++ sql/40alter-table.sql
@@ -12,11 +12,27 @@ CREATE TABLE my_table46 (
 ALTER TABLE my_table46
 MODIFY col1 INT NOT NULL ;
 
-DESC my_table46;
 
 -- NOT NULL 제약사항 삭제
 ALTER TABLE my_table46
 MODIFY col1 INT NULL;
 
 -- 예) col2에 not null 제약사항 추가
+ALTER TABLE my_table46
+MODIFY col2 INT NOT NULL ;
 -- 예) col2에 not null 제약사항 삭제
+ALTER TABLE my_table46
+MODIFY col2 INT NULL ;
+
+-- DEFAULT 제약사항 추가
+ALTER TABLE my_table46
+ALTER col3 SET DEFAULT 100;
+
+
+-- DEFAULT 제약사항 삭제
+ALTER TABLE my_table46
+ALTER col3 DROP DEFAULT ;
+
+DESC my_table46;
+
+-- 예) 
