0daaadcdd51a6905388db0d883f64b5058220632
sebaek <sebaek@gmail.com>
2023-10-16 09:37:49 +0900

    집계 함수

--- sql/36aggregate-function.sql
+++ sql/36aggregate-function.sql
@@ -19,6 +19,37 @@ SELECT * FROM products
 WHERE Price = (SELECT MIN(Price) FROM products);
 
 -- 예) 가장 어린 직원 명 조회
+SELECT * FROM employees
+WHERE BirthDate = (SELECT MAX(BirthDate) FROM employees);
 -- 예) 가장 나이 많은 직원명 조회
+SELECT *
+FROM employees
+WHERE BirthDate = (SELECT MIN(BirthDate) FROM employees);
+
+-- COUNT : 갯수
+SELECT * FROM employees;
+SELECT COUNT(EmployeeID) FROM employees;
+SELECT COUNT(LastName) FROM employees;
+SELECT COUNT(FirstName) FROM employees;
+
+--  NULL은 포함하지 않음
+INSERT INTO employees (LastName)
+VALUE ('흥민');
+SELECT * FROM employees ORDER BY 1 DESC;
+
+SELECT MIN(BirthDate) FROM employees;
+SELECT MAX(BirthDate) FROM employees;
+
+INSERT INTO products (ProductName)
+VALUE ('커피');
+
+SELECT * FROM products ORDER BY 1 DESC ;
+SELECT MIN(Price) FROM products;
+SELECT MIN(IFNULL(Price, 0)) FROM products;
+SELECT *
+FROM products
+WHERE IFNULL(Price, 0) = (SELECT MIN(IFNULL(Price, 0)) FROM products);
+
+
 
 
