fdd7c5046108577d5ccac1d29a2e25042c6c0bb4
sebaek <sebaek@gmail.com>
2023-10-17 14:33:53 +0900

    di

--- src/main/java/com/example/spring20230920/MyApp12.java
+++ src/main/java/com/example/spring20230920/MyApp12.java
@@ -1,8 +1,11 @@
 package com.example.spring20230920;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class MyApp12 {
@@ -18,16 +21,30 @@ public class MyApp12 {
         System.out.println(bean2.hashCode()); // int값 출력 (1)
         System.out.println(o1.getField().hashCode()); // int값 출력 (1)
 
+        // (1)끼리는 같은 값 출력되도록....
+
     }
 }
 
 interface MyInterface3 {}
 
+@Component
 class MyClass23 implements MyInterface3 {}
+@Component
 class MyClass24 implements MyInterface3 {}
 
+@Component
 class MyClass25 {
     private MyInterface3 field;
+
+    public MyClass25(@Qualifier("myClass23") MyInterface3 field) {
+        this.field = field;
+    }
+
+    //    @Autowired
+//    public void setField(@Qualifier("myClass23") MyInterface3 field) {
+//        this.field = field;
+//    }
 
     public MyInterface3 getField() {
         return field;
