3be098ea2de479d8f65a3f2685808ffd23ab7e41
sebaek <sebaek@gmail.com>
2023-10-17 14:21:39 +0900

    di

---
+++ src/main/java/com/example/spring20230920/MyApp12.java
@@ -0,0 +1,35 @@
+package com.example.spring20230920;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class MyApp12 {
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(MyApp12.class, args);
+        Object bean = context.getBean("myClass25");
+        Object bean1 = context.getBean("myClass24");
+        Object bean2 = context.getBean("myClass23");
+
+        MyClass25 o1 = (MyClass25) bean;
+
+        System.out.println(bean1.hashCode()); // int값 출력
+        System.out.println(bean2.hashCode()); // int값 출력 (1)
+        System.out.println(o1.getField().hashCode()); // int값 출력 (1)
+
+    }
+}
+
+interface MyInterface3 {}
+
+class MyClass23 implements MyInterface3 {}
+class MyClass24 implements MyInterface3 {}
+
+class MyClass25 {
+    private MyInterface3 field;
+
+    public MyInterface3 getField() {
+        return field;
+    }
+}
