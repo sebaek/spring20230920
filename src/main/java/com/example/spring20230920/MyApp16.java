df6e3339399c75d653b7500786f7aa22f50820b3
sebaek <sebaek@gmail.com>
2023-10-17 15:48:31 +0900

    spring bean

--- src/main/java/com/example/spring20230920/MyApp16.java
+++ src/main/java/com/example/spring20230920/MyApp16.java
@@ -3,6 +3,8 @@ package com.example.spring20230920;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class MyApp16 {
@@ -25,11 +27,29 @@ public class MyApp16 {
     }
 }
 
+@Configuration
+class Configuration3 {
+
+    @Bean
+    public MyClass31 myClass31Bean() {
+        return new MyClass31();
+    }
+
+    @Bean
+    public MyClass30 myClass30Bean() {
+        return new MyClass30(myClass31Bean());
+    }
+}
+
 class MyClass31 {
 
 }
 class MyClass30 {
     private MyClass31 field;
+
+    public MyClass30(MyClass31 field) {
+        this.field = field;
+    }
 
     public MyClass31 getField() {
         return field;
