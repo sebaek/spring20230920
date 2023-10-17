33b33085454154c2827a8e9028e3caf555f895de
sebaek <sebaek@gmail.com>
2023-10-17 11:23:14 +0900

    component

--- src/main/java/com/example/spring20230920/MyApp3.java
+++ src/main/java/com/example/spring20230920/MyApp3.java
@@ -3,7 +3,6 @@ package com.example.spring20230920;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication
@@ -14,6 +13,9 @@ public class MyApp3 {
         MyClass5 bean1 = context.getBean("myClass5", MyClass5.class);
         System.out.println("bean = " + bean);
         System.out.println("bean1 = " + bean1);
+
+        Object bean2 = context.getBean("controller02");
+        System.out.println("bean2 = " + bean2);
     }
 }
 @Component // spring bean의 이름을 명시하지 않으면 클래스이름 -> lowerCamelCase
