9e7fbc686d6c810226ac581ffedc336f9611f648
sebaek <sebaek@gmail.com>
2023-10-20 11:39:31 +0900

    service

--- src/main/java/com/example/spring20230920/service/MyService1.java
+++ src/main/java/com/example/spring20230920/service/MyService1.java
@@ -8,15 +8,15 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(rollbackFor = Exception.class)
 public class MyService1 {
 
     private final MyDao6 dao;
 
-    @Transactional
     public void tx1() {
         dao.update1();
 
-//        int c = 1 / 0; // runtimeException
+        int c = 1 / 0; // runtimeException
 
         dao.update2();
     }
