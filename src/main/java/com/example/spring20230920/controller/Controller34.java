585ae29b63233507531064f1092c66e58086af05
sebaek <sebaek@gmail.com>
2023-10-23 09:38:11 +0900

    add s3 dependency

---
+++ src/main/java/com/example/spring20230920/controller/Controller34.java
@@ -0,0 +1,26 @@
+package com.example.spring20230920.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+@RequestMapping("main34")
+@RequiredArgsConstructor
+public class Controller34 {
+
+    private final S3Client s3Client;
+
+    @GetMapping("sub1")
+    public void method1() {
+
+    }
+
+    @PostMapping("sub2")
+    public void method2(MultipartFile file) {
+//        s3Client.putObject();
+    }
+}
