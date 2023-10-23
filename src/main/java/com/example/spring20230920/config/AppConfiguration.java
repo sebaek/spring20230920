73b7a64f1485ad8b6ec1ba95ff5529acac73628c
sebaek <sebaek@gmail.com>
2023-10-23 09:53:35 +0900

    s3client bean

---
+++ src/main/java/com/example/spring20230920/config/AppConfiguration.java
@@ -0,0 +1,29 @@
+package com.example.spring20230920.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class AppConfiguration {
+    @Value("${aws.accessKeyId}")
+    private String accessKeyId;
+    @Value("${aws.secretAccessKey}")
+    private String secretAccessKey;
+
+    @Bean
+    public S3Client s3Client() {
+        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKeyId, secretAccessKey);
+        AwsCredentialsProvider provider = StaticCredentialsProvider.create(credentials);
+
+        S3Client.builder()
+                .region(Region.AP_NORTHEAST_2)
+                .credentialsProvider(provider)
+                .build();
+    }
+}
--- src/main/java/com/example/spring20230920/controller/Controller34.java
+++ src/main/java/com/example/spring20230920/controller/Controller34.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.services.s3.S3Client;
 
 @Controller
 @RequestMapping("main34")
