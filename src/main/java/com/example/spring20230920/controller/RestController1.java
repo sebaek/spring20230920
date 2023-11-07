5536610e291a89908ad6836a598b33658cd4795f
sebaek <sebaek@gmail.com>
2023-11-07 16:37:18 +0900

    api/main1/sub1

--- src/main/java/com/example/spring20230920/controller/RestController1.java
+++ src/main/java/com/example/spring20230920/controller/RestController1.java
@@ -1,6 +1,7 @@
 package com.example.spring20230920.controller;
 
 import com.example.spring20230920.dao.MyDao10;
+import com.example.spring20230920.domain.MyDto34Customer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,5 +29,11 @@ public class RestController1 {
     @ResponseBody
     public String method2() {
         return dao.getCustomerName();
+    }
+
+    @GetMapping("sub3")
+    @ResponseBody
+    public MyDto34Customer method3() {
+        return dao.getCustomer();
     }
 }
--- src/main/java/com/example/spring20230920/dao/MyDao10.java
+++ src/main/java/com/example/spring20230920/dao/MyDao10.java
@@ -1,5 +1,6 @@
 package com.example.spring20230920.dao;
 
+import com.example.spring20230920.domain.MyDto34Customer;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -11,4 +12,18 @@ public interface MyDao10 {
         WHERE CustomerID = 50
         """)
     String getCustomerName();
+
+    @Select("""
+        SELECT 
+         customerId id,
+         customerName name,
+         contactName,
+         address,
+         city,
+         postalCode, 
+         country
+         FROM customers
+        WHERE customerId = 50
+        """)
+    MyDto34Customer getCustomer();
 }
